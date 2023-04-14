b94be8a62ed32ec5ae46c5eac58467255ea59442
Sumaiya <sumaiya-ahmed95@outlook.com>
2023-04-14 11:24:52 +0100

    add interest component

--- Solo Project 1/src/App.js
+++ Solo Project 1/src/App.js
@@ -1,11 +1,13 @@
 import Info from "./components/Info.js";
 import About from "./components/About.js";
+import Interest from "./components/Interest.js";
 
 export default function App() {
   return (
     <div>
       <Info />
       <About />
+      <Interest />
     </div>
   );
 }
---
+++ Solo Project 1/src/components/Interest.js
@@ -0,0 +1,11 @@
+export default function Interest() {
+  return (
+    <div>
+      <h2 className="interest-heading">Interests</h2>
+      <p className="interest-paragraph">
+        Food expert. Music scholar. Reader. Internet fanatic. Bacon buff.
+        Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.
+      </p>
+    </div>
+  );
+}
