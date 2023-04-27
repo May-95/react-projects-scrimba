31075da74d2a02554a3c4c2c6d500aa851126bd6
Sumaiya <sumaiya-ahmed95@outlook.com>
2023-04-27 23:46:18 +0100

    add navbar component

---
+++ Solo Project 2/src/components/Navbar.js
@@ -0,0 +1,13 @@
+export default function Navbar() {
+    return (
+      <header>
+        <nav className="navbar-component">
+          <i
+            className="fa-solid fa-earth-americas fa-xl"
+            style={{ color: "#ffffff", fontSize: "1.9em" }}
+          ></i>
+          <p className="journal-title">my travel journal</p>
+        </nav>
+      </header>
+    );
+  }
